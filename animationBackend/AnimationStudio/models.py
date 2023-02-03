43886d7902ce818f7cf3b3081a11e41588841e48
Will Powers <wgpowers98@yahoo.com>
2023-02-02 21:43:43 -0500

    Updates

--- animationBackend/AnimationStudio/models.py
+++ animationBackend/AnimationStudio/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class AnimationData(models.Model):
     frame = models.TextField()
     FRID = models.CharField(max_length=6)
+    Title = models.CharField(max_length=40)
 
 
 
