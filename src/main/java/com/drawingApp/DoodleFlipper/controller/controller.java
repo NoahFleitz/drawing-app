0ab4a25576c5f66702248048741b129e08389067
Will Powers <wgpowers98@yahoo.com>
2024-03-22 01:14:56 -0400

    Updates

--- src/main/java/com/drawingApp/DoodleFlipper/controller/controller.java
+++ src/main/java/com/drawingApp/DoodleFlipper/controller/controller.java
@@ -1,13 +1,19 @@
 package com.drawingApp.DoodleFlipper.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 public class controller 
-@Controller
+
 {
    @GetMapping("/")
-    public String homePage() 
+   
+    public String homePage(Model Model) 
    {
-
+   return("<H1>Hello World</H1>");
    } 
 }
